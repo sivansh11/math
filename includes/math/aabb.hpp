e8f93ee68b5f80b278d9d3a1151e54a22705b9f3
sivansh11 <sivanshgupta2002@gmail.com>
2025-09-22 02:47:35 +0530

    added helper functions

--- includes/math/aabb.hpp
+++ includes/math/aabb.hpp
@@ -12,10 +12,13 @@ namespace math {
 struct aabb_t {
   bool is_valid() const;
   vec3 extent() const;
+  float largest_extent() const;
   float area() const;
   vec3 center() const;
+  uint32_t largest_axis() const;
   aabb_t &grow(const glm::vec3 &point);
   aabb_t &grow(const aabb_t &aabb);
+  aabb_t &shrink(const aabb_t& aabb);
 
   glm::vec3 min{infinity};
   glm::vec3 max{-infinity};
--- src/aabb.cpp
+++ src/aabb.cpp
@@ -9,6 +9,10 @@ glm::vec3 aabb_t::extent() const {
   assert(is_valid());
   return max - min;
 }
+float aabb_t::largest_extent() const {
+  const glm::vec3 e = extent();
+  return glm::max(e.x, glm::max(e.y, e.z));
+}
 float aabb_t::area() const {
   assert(is_valid());
   const glm::vec3 e = extent();
@@ -18,6 +22,13 @@ glm::vec3 aabb_t::center() const {
   assert(is_valid());
   return (max + min) / 2.f;
 }
+uint32_t aabb_t::largest_axis() const {
+  const glm::vec3 e = extent();
+  uint32_t axis = 0;
+  if (e[0] < e[1]) axis = 1;
+  if (e[axis] < e[2]) axis = 2;
+  return axis;
+}
 aabb_t &aabb_t::grow(const glm::vec3 &point) {
   min = glm::min(min, point);
   max = glm::max(max, point);
@@ -26,6 +37,11 @@ aabb_t &aabb_t::grow(const glm::vec3 &point) {
 aabb_t &aabb_t::grow(const aabb_t &aabb) {
   min = glm::min(min, aabb.min);
   max = glm::max(max, aabb.max);
+  return *this;
+}
+aabb_t &aabb_t::shrink(const aabb_t& aabb) {
+  min = glm::max(min, aabb.min);
+  max = glm::min(max, aabb.max);
   return *this;
 }
 
