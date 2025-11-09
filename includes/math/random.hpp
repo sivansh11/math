4812d9096ff4fb219ce2f15840e87f0bcc6b8998
sivansh11 <sivanshgupta2002@gmail.com>
2025-11-09 20:09:38 +0530

    added min/max random

--- includes/math/random.hpp
+++ includes/math/random.hpp
@@ -8,13 +8,15 @@
 namespace math {
 
 struct random_t {
-  float random_float() { return distribution(generator); }
+  float random_float(float min, float max) {
+    return min + (max - min) * distribution(generator);
+  }
 
   template <length_t L, typename T = float, qualifier Q = defaultp>
-  constexpr math::vec<L, T, Q> random_vec() {
+  constexpr math::vec<L, T, Q> random_vec(float min, float max) {
     math::vec<L, T, Q> v{};
     for (int32_t axis = 0; axis < L; axis++) {
-      v[axis] = random_float();
+      v[axis] = random_float(min, max);
     }
     return v;
   }
