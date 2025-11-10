66128a8e568994162a49f1e237dcf9c6f903cd14
sivansh11 <sivanshgupta2002@gmail.com>
2025-11-10 09:29:12 +0530

    added generic ostream overloads

--- includes/math/math.hpp
+++ includes/math/math.hpp
@@ -19,8 +19,15 @@ static constexpr float infinity = std::numeric_limits<float>::max();
 
 }  // namespace math
 
-std::ostream &operator<<(std::ostream &o, const math::vec2 &v);
-std::ostream &operator<<(std::ostream &o, const math::vec3 &v);
-std::ostream &operator<<(std::ostream &o, const math::vec4 &v);
+template <math::length_t L, typename T, math::qualifier Q>
+std::ostream &operator<<(std::ostream &o, const math::vec<L, T, Q> &v) {
+  o << math::to_string(v);
+  return o;
+}
+template <math::length_t C, math::length_t R, typename T, math::qualifier Q>
+std::ostream &operator<<(std::ostream &o, const math::mat<C, R, T, Q> &v) {
+  o << math::to_string(v);
+  return o;
+}
 
 #endif  // !MATH_HPP
--- src/math.cpp
+++ src/math.cpp
@@ -1,14 +1,1 @@
 #include "math/math.hpp"
-
-std::ostream &operator<<(std::ostream &o, const math::vec2 &v) {
-  o << math::to_string(v);
-  return o;
-}
-std::ostream &operator<<(std::ostream &o, const math::vec3 &v) {
-  o << math::to_string(v);
-  return o;
-}
-std::ostream &operator<<(std::ostream &o, const math::vec4 &v) {
-  o << math::to_string(v);
-  return o;
-}
