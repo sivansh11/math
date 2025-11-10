da713ef9b050ac7acdb9dcbf3d1b4316200e33ac
sivansh11 <sivanshgupta2002@gmail.com>
2025-11-10 09:37:52 +0530

    added generic fmt

--- examples/test/main.cpp
+++ examples/test/main.cpp
@@ -1,3 +1,9 @@
-#include "math/aabb.hpp"
+#include "math/math.hpp"
+#include <format>
+#include <iostream>
 
-int main() { return 0; }
+int main() {
+  math::vec3 v{1, 2, 3};
+  std::cout << std::format("{}", v) << '\n';
+
+}
--- includes/math/math.hpp
+++ includes/math/math.hpp
@@ -2,6 +2,7 @@
 #define MATH_HPP
 
 #include <ostream>
+#include <format>
 
 #define GLM_ENABLE_EXPERIMENTAL
 #define GLM_DEPTH_ZERO_TO_ONE
@@ -29,5 +30,29 @@ std::ostream &operator<<(std::ostream &o, const math::mat<C, R, T, Q> &v) {
   o << math::to_string(v);
   return o;
 }
+
+template <math::length_t L, typename T, math::qualifier Q>
+struct std::formatter<math::vec<L, T, Q>> {
+  template <typename parse_context_t>
+  constexpr auto parse(parse_context_t &ctx) {
+    return ctx.begin();
+  }
+  template <typename format_context_t>
+  auto format(const math::vec<L, T, Q> &v, format_context_t &ctx) const {
+    return std::format_to(ctx.out(), "{}", math::to_string(v));
+  }
+};
+
+template <math::length_t C, math::length_t R, typename T, math::qualifier Q>
+struct std::formatter<math::mat<C, R, T, Q>> {
+  template <typename parse_context_t>
+  constexpr auto parse(parse_context_t &ctx) {
+    return ctx.begin();
+  }
+  template <typename format_context_t>
+  auto format(const math::mat<C, R, T, Q> &v, format_context_t &ctx) const {
+    return std::format_to(ctx.out(), "{}", math::to_string(v));
+  }
+};
 
 #endif  // !MATH_HPP
